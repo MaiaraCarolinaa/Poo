132fd9ee780259c789ac3907dc6bdcc5670f5e89
MaiaraCarolinaa <maiaracarolina.silva@gmail.com>
2026-04-07 11:13:13 -0400

    Exercicio 2

--- Carro.java
+++ Carro.java
@@ -4,13 +4,14 @@ public class Carro {
         carro1.marca = "Toyota";
         carro1.modelo = "Corolla";
         carro1.ano = 2024;
-        System.out.println("Marca: " + carro1.marca + ", Modelo: " + carro1.modelo + ", Ano: " + carro1.ano);
+        carro1.exibir();
+        carro1.idadeDoCarro();
 
         Tipo carro2 = new Tipo();
         carro2.marca = "Fiat";
         carro2.modelo = "Toro";
         carro2.ano = 2017;
-        System.out.println("Marca: " + carro2.marca + ", Modelo: " + carro2.modelo + ", Ano: " + carro2.ano);
+        carro2.exibir();
     }
 
 }
--- Tipo.java
+++ Tipo.java
@@ -2,4 +2,18 @@ public class Tipo {
     String marca;
     String modelo;
     int ano;
+
+
+public int idadeDoCarro(){
+    return 2026 - ano;
+}
+
+public void exibir(){
+        System.out.println("Marca: " + this.marca);
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Ano: " + this.ano);
+        System.out.println("A idade do carro é: " + idadeDoCarro());
+
+    }
+
 }
