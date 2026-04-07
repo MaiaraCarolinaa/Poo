8ab1376479d4f2baf5672cacf367ad07ea78ca69
MaiaraCarolinaa <maiaracarolina.silva@gmail.com>
2026-04-07 13:11:15 -0400

    Exercicio 3

--- Carro.java
+++ Carro.java
@@ -1,17 +1,35 @@
+
+import java.util.ArrayList;
+
 public class Carro {
     public static void main(String[] args){
+        ArrayList<Tipo> listadecarros = new ArrayList<>();
+
         Tipo carro1 = new Tipo();
         carro1.marca = "Toyota";
         carro1.modelo = "Corolla";
         carro1.ano = 2024;
-        carro1.exibir();
-        carro1.idadeDoCarro();
+        listadecarros.add(carro1);
 
         Tipo carro2 = new Tipo();
         carro2.marca = "Fiat";
         carro2.modelo = "Toro";
         carro2.ano = 2017;
-        carro2.exibir();
+        listadecarros.add(carro2);
+
+        Tipo carro3 = new Tipo();
+        carro3.marca = "Volkswagen";
+        carro3.modelo = "Fusca";
+        carro3.ano = 2001;
+        listadecarros.add(carro3);
+
+        System.out.println("--------- LISTA DE CARROS -------------");
+        
+        for (Tipo t : listadecarros){
+            t.exibir();
+
+        System.out.println("-----------------------");
+        }
     }
 
 }
