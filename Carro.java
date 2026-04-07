0f39c27f0b34e63503de249434d3bea7e06abfee
MaiaraCarolinaa <maiaracarolina.silva@gmail.com>
2026-04-07 15:02:37 -0400

    Exercicio 4

--- Carro.java
+++ Carro.java
@@ -1,35 +1,17 @@
 
-import java.util.ArrayList;
-
 public class Carro {
     public static void main(String[] args){
-        ArrayList<Tipo> listadecarros = new ArrayList<>();
 
-        Tipo carro1 = new Tipo();
-        carro1.marca = "Toyota";
-        carro1.modelo = "Corolla";
-        carro1.ano = 2024;
-        listadecarros.add(carro1);
+        Tipo carro1= new Tipo();
+        carro1.setMarca("Chevrolet");
+        carro1.setModelo("Silverado");
+        carro1.setAno(2025);
+        carro1.exibir();
 
         Tipo carro2 = new Tipo();
-        carro2.marca = "Fiat";
-        carro2.modelo = "Toro";
-        carro2.ano = 2017;
-        listadecarros.add(carro2);
-
-        Tipo carro3 = new Tipo();
-        carro3.marca = "Volkswagen";
-        carro3.modelo = "Fusca";
-        carro3.ano = 2001;
-        listadecarros.add(carro3);
-
-        System.out.println("--------- LISTA DE CARROS -------------");
-        
-        for (Tipo t : listadecarros){
-            t.exibir();
-
-        System.out.println("-----------------------");
+        carro2.setMarca ("Volkswagen");
+        carro2.setModelo ("Gol");
+        carro2.setAno (1998);
+        carro2.exibir();
         }
     }
-
-}
--- Tipo.java
+++ Tipo.java
@@ -1,19 +1,38 @@
 public class Tipo {
-    String marca;
-    String modelo;
-    int ano;
+    private String marca;
+    private String modelo;
+    private int ano;
 
+public String getMarca(){
+        return this.marca;
+    }
+    public void setMarca(String m){ //void pq ele não vai retornar nada
+        this.marca = m;
+    }
 
-public int idadeDoCarro(){
-    return 2026 - ano;
-}
+public String getModelo(){
+        return this.modelo;
+    }
+    public void setModelo(String md){
+        this.modelo = md;
+    }
 
-public void exibir(){
-        System.out.println("Marca: " + this.marca);
-        System.out.println("Modelo: " + this.modelo);
-        System.out.println("Ano: " + this.ano);
-        System.out.println("A idade do carro é: " + idadeDoCarro());
+    public int getAno(){
+        return this.ano;
+    }
+    public void setAno(int a){
+        // validação
+        if (a < 1886 || a > 2026){
+            System.out.println("404 ! Ano inválido");
+        } else{
+            this.ano = a;
+        }
+    }
 
+    public void exibir(){
+        System.out.println("Marca: " + this.getMarca());
+        System.out.println("Modelo: " + this.getModelo());
+        System.out.println("Ano: " + (this.getAno() == 0 ? "Não informado" : this.getAno()));
     }
 
 }
